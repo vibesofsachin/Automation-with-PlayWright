aea1a01d8e5178f54ca924f88ec1a2f8b26c43f6
Sachin Tripathi <vibesofsachin@gmail.com>
2026-06-08 13:10:45 +0530

    allure commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'],['allure-playwright]],
+reporter: [    ['html'],
+    ['allure-playwright']
+],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
 
